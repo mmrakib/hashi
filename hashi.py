40f26d4586656c28a25a4c3ad20452aea203c5ac
Mayaz Rakib <mohammadmrakib@gmail.com>
2024-03-13 04:07:11 +1100

    Added generate bridge configurations function

--- hashi.py
+++ hashi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+from itertools import product, combinations
 
 def load_grid():
     grid = []
@@ -12,7 +13,29 @@ def load_grid():
             else:
                 rowlist.append(0)
         grid.append(rowlist)
-    return grid
+    nrows = len(grid)
+    ncols = len(grid[0])
+    return grid, nrows, ncols
+
+def generate_bridge_configurations(grid, nrows, ncols):
+    bridges = []
+    for point1, point2 in combinations(product(range(nrows), range(ncols)), 2):
+        row1, col1 = point1
+        row2, col2 = point2
+        if grid[row1][col1] and grid[row2][col2]:
+            if row1 == row2 and abs(col1 - col2) > 1:
+                is_horizontal = True
+                gap_range = range(min(col1, col2) + 1, max(col1, col2))
+                is_valid = all(grid[row1][col] == 0 for col in gap_range)
+                if is_valid:
+                    bridges.append((point1, point2, [row1], gap_range, is_horizontal))    
+            if col1 == col2 and abs(row1 - row2) > 1:
+                is_horizontal = False
+                gap_range = range(min(row1, row2) + 1, max(row1, row2))
+                is_valid = all(grid[row][col1] == 0 for row in gap_range)
+                if is_valid:
+                    bridges.append((point1, point2, gap_range, [col1], is_horizontal))
+    return bridges
 
 def print_grid(grid):
     for row in grid:
@@ -24,5 +47,9 @@ def print_grid(grid):
         print('')
 
 if __name__ == '__main__':
-    grid = load_grid()
+    grid, nrows, ncols = load_grid()
     print_grid(grid)
+    bridges = generate_bridge_configurations(grid, nrows, ncols)
+    print('Bridges')
+    for b in bridges:
+        print(b)
